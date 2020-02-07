d9b0a03c5400dc508d316a62f28badba738b0c40
Tuhin Sarkar <tuhin_sarkar@srmap.edu.in>
2020-02-07 12:02:33 +0530

    menu driven binary search

--- Exercise2/c.c
+++ Exercise2/c.c
@@ -28,48 +28,67 @@ int main()
     indf = fopen("indexs.txt", "r");
     data = fopen("data.txt", "r");
 
-    int key = 0, flag = 1;
-    printf("Enter the roll number you are looking for: ");
-    scanf("%d", &key);
-
-    clock_t begin = clock();
-
-    f = 0;
-
-    l = 1000000;
-
-    while (f <= l)
+    while (1)
     {
-        m = (f+l)/2;
 
-        fseek(indf, m*sizeof(struct IndFile), SEEK_SET);
-        fread(&ind, sizeof(struct IndFile), 1, indf);
-        
-        if (ind.rollno == key){ 
-            fseek(data, ind.offset * sizeof(struct Student), SEEK_SET);
-            fread(&stud, sizeof(struct Student), 1, data);
-            printf("Rollno = %d    Name = %s    Age = %d    Branch = %s\n", stud.rollno, stud.name, stud.age, stud.branch);
-            flag = 0;
-            break;
+        int key = 0, flag = 1, ex;
+        printf("Press 1 to perform binary search\nPress 0 to exit\n");
+        scanf("%d", &ex);
+
+        if (ex)
+        {
+
+            printf("Enter the roll number you are looking for: ");
+            scanf("%d", &key);
+
+            clock_t begin = clock();
+
+            f = 0;
+
+            l = 1000000;
+
+            while (f <= l)
+            {
+                m = (f + l) / 2;
+
+                fseek(indf, m * sizeof(struct IndFile), SEEK_SET);
+                fread(&ind, sizeof(struct IndFile), 1, indf);
+
+                if (ind.rollno == key)
+                {
+                    fseek(data, ind.offset * sizeof(struct Student), SEEK_SET);
+                    fread(&stud, sizeof(struct Student), 1, data);
+                    printf("Rollno = %d    Name = %s    Age = %d    Branch = %s\n", stud.rollno, stud.name, stud.age, stud.branch);
+                    flag = 0;
+                    break;
+                }
+
+                else if (ind.rollno < key)
+                {
+                    f = m + 1;
+                }
+
+                else
+                {
+                    l = m - 1;
+                }
+            }
+
+            if (flag)
+            {
+                printf("Not present \n");
+            }
+
+            clock_t end = clock();
+
+            double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+            printf("Time elpased is %f seconds\n", time_spent);
         }
 
-        else if (ind.rollno < key){
-            f = m+1;
-        }
-
-        else{
-            l = m-1;
+        if (ex == 0)
+        {
+            exit(0);
         }
     }
-
-    if (flag)
-    {
-        printf("Not present \n");
-    }
-
-    clock_t end = clock();
-
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-
-    printf("Time elpased is %f seconds", time_spent);
 }
