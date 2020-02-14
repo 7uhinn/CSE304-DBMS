4956fdfc1cf4cfac7151e59e3f3c7fd737c0a687
Tuhin Sarkar <tuhin_sarkar@srmap.edu.in>
2020-02-14 09:38:56 +0530

    unique roll nos.

--- Exercise3/a.c
+++ Exercise3/a.c
@@ -32,20 +32,47 @@ int generation(char *s, int len)
 
 int main()
 {
-    FILE *f1;
-    struct Student stud1;
+    FILE *f1, *f2;
+    struct Student stud;
+    struct Index ind;
+    int roll[20];
+    int temp;
+    int f = 0, k = 0, s = 0;
 
     f1 = fopen("data.txt", "w");
+    f2 = fopen("index.txt", "w");
 
     int n = 20;
 
     printf("Generating file...");
     for (int i = 0; i < n; i++)
     {
-        generation(stud1.name, 5);
-        stud1.rollno = (rand() % (1000000));
-        stud1.age = (rand() % (50));
-        strcpy(stud1.branch, "CSE");
-        fwrite(&stud1, sizeof(struct Student), 1, f1);
+        generation(stud.name, 5);
+        stud.age = (rand() % (50));
+        strcpy(stud.branch, "CSE");
+
+        while (s == 0)
+        {
+            temp = (rand() % (100));
+            for (int j = 0; j < sizeof(roll); j++)
+            {
+                if (temp == roll[j])
+                {
+                    f = 1;
+                    break;
+                }
+            }
+            if (f==0)
+            {
+                roll[k] = temp;
+                stud.rollno = temp;
+                k++;
+                s = 1;
+            }
+            f = 0;
+        }
+
+        fwrite(&stud, sizeof(struct Student), 1, f1);
+        s = 0;
     }
 }
