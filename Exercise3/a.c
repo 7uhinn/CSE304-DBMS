689fd1b50ee455f14f5363b0e4e828f97140b5c7
Tuhin Sarkar <tuhin_sarkar@srmap.edu.in>
2020-02-15 00:51:07 +0530

    BST built in file

--- Exercise3/a.c
+++ Exercise3/a.c
@@ -34,17 +34,17 @@ int main()
 {
     FILE *f1, *f2;
     struct Student stud;
-    struct Index ind;
+    struct Index ind[20];
     int roll[20];
     int temp;
-    int f = 0, k = 0, s = 0;
+    int f = 0, k = 0, s = 0, m = 0, z = 0;
 
     f1 = fopen("data.txt", "w");
     f2 = fopen("index.txt", "w");
 
     int n = 20;
 
-    printf("Generating file...");
+    printf("Generating file...\n");
     for (int i = 0; i < n; i++)
     {
         generation(stud.name, 5);
@@ -62,7 +62,7 @@ int main()
                     break;
                 }
             }
-            if (f==0)
+            if (f == 0)
             {
                 roll[k] = temp;
                 stud.rollno = temp;
@@ -71,8 +71,63 @@ int main()
             }
             f = 0;
         }
-
+        printf("Rollno = %d    Name = %s    Age = %d    Branch = %s\n", stud.rollno, stud.name, stud.age, stud.branch);
         fwrite(&stud, sizeof(struct Student), 1, f1);
         s = 0;
+
+        if (i == 0)
+        {
+            ind[0].rollno = stud.rollno;
+            ind[0].lc = -1;
+            ind[0].rc = -1;
+            ind[0].offset = i;
+        }
+
+        else
+        {
+            z = 0;
+            m = 0;
+            while (m == 0)
+            {
+                if (stud.rollno < ind[z].rollno)
+                {
+                    if (ind[z].lc == -1)
+                    {
+                        ind[z].lc = i;
+                        m = 1;
+                    }
+                    else
+                    {
+                        z = ind[z].lc;
+                    }
+                }
+                else
+                {
+                    if (stud.rollno > ind[z].rollno)
+                    {
+                        if (ind[z].rc == -1)
+                        {
+                            ind[z].rc = i;
+                            m = 1;
+                        }
+                        else
+                        {
+                            z = ind[z].rc;
+                        }
+                    }
+                }
+            }
+
+            ind[i].rollno = stud.rollno;
+            ind[i].lc = -1;
+            ind[i].rc = -1;
+            ind[i].offset = i;
+        }
+    }
+    printf(".....................................................................\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Rollno = %d    LC = %d    RC = %d    Offset = %d\n", ind[i].rollno, ind[i].lc, ind[i].rc, ind[i].offset);
+        fwrite(&ind[i], sizeof(struct Index), 1, f2);
     }
 }
