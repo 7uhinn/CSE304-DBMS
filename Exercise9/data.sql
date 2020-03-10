e47988d5b6ab2ebe227387a20d3095e6cf2dd407
Tuhin <tuhin_sarkar@srmap.edu.in>
2020-03-10 10:48:23 +0530

    data updated

--- Exercise9/data.sql
+++ Exercise9/data.sql
@@ -1,29 +1,24 @@
 INSERT INTO Suppliers
     (sid,sname,address)
 VALUES
-    (1, 'Ramesh', 'Ahmedabad'),
-    (2, 'Khilan', 'Delhi'),
-    (3, 'Kaushik', 'Chennai'),
-    (4, 'Chaitali', 'Mumbai'),
-    (5, 'Hardik', 'Bhopal');
+    (1, 'Sarala', 'Bombay'),
+    (2, 'Uma', 'Chennai'),
+    (3, 'Nehru', 'Chennai'),
+    (4, 'Priya', 'Bombay'),
+    (5, 'Anand', 'Delhi');
 
 SELECT *
 FROM Suppliers;
 
-
 INSERT INTO Parts
     (pid,pname,color)
 VALUES
-    (1, 'One', 'blue'),
-    (2, 'Two', 'green'),
-    (3, 'Three', 'red'),
-    (4, 'Four', 'black'),
-    (5, 'Five', 'red'),
-    (6, 'Six', 'blue'),
-    (7, 'Seven', 'green'),
-    (8, 'Eight', 'red'),
-    (9, 'Nine', 'black'),
-    (10, 'Ten', 'red');
+    (1, 'Nut', 'red'),
+    (2, 'Bolt', 'green'),
+    (3, 'Screw', 'blue'),
+    (4, 'Screw', 'red'),
+    (5, 'Cam', 'blue'),
+    (6, 'Cog', 'red');
 
 SELECT *
 FROM Parts;
@@ -31,27 +26,18 @@ FROM Parts;
 INSERT INTO Catalog
     (sid,pid,cost)
 VALUES
-    (1, 4, 250),
-    (2, 3, 85),
-    (3, 3, 105),
-    (3, 5, 733),
-    (4, 2, 912),
-    (5, 1, 436),
-    (1, 8, 2122),
-    (3, 8, 1543),
-    (1, 10, 1322),
-    (3, 10, 1222),
-    (2, 7, 2222),
-    (4, 6, 599),
-    (4, 9, 1423),
-    (1, 3, 121),
-    (1, 5, 699),
-    (1, 2, 899),
-    (1, 1, 1122),
-    (1, 7, 783),
-    (1, 6, 510),
-    (1, 9, 1423);
-
+    (1, 1, 300),
+    (1, 2, 200),
+    (1, 3, 400),
+    (1, 4, 200),
+    (1, 5, 100),
+    (1, 6, 100),
+    (2, 1, 300),
+    (2, 2, 400),
+    (3, 2, 400),
+    (4, 2, 200),
+    (4, 4, 300),
+    (4, 5, 400);
 
 SELECT *
 FROM Catalog;
