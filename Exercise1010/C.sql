c7ebf6eb09b846e9c25cb885cea5873f8165fd81
Tuhin <tuhin_sarkar@srmap.edu.in>
2020-04-04 00:22:13 +0530

    ex10 completed

---
+++ Exercise1010/C.sql
@@ -0,0 +1,15 @@
+BEGIN
+
+    INSERT INTO Department
+        (Dname,Dnumber,Mgr_Ssn,Mgr_start_date)
+    VALUES
+        ('NewDep', 2, 281198123, '1983-04-12');
+
+    INSERT INTO Employee
+        (Fname,Minit,Lname,Ssn,Bdate,Address,Sex,Salary,Super_ssn,Dno)
+    VALUES
+        ('Jake', 'B', 'Peralta', 281198123, '1956-01-09', '731 Baxton, Austin, TX', 'M', 40000, NULL, 2);
+
+    COMMIT;
+
+END;
---
+++ Exercise1010/Calt.sql
@@ -0,0 +1,26 @@
+INSERT INTO Employee
+    (Fname,Minit,Lname,Ssn,Bdate,Address,Sex,Salary,Super_ssn,Dno)
+VALUES
+    ('Jake', 'B', 'Peralta', 281198123, '1956-01-09', '731 Baxton, Austin, TX', 'M', 40000, NULL, NULL);
+
+INSERT INTO Department
+    (Dname,Dnumber,Mgr_Ssn,Mgr_start_date)
+VALUES
+    ('NewDep', NULL, 281198123, '1983-04-12');
+
+INSERT INTO Employee
+    (Fname,Minit,Lname,Ssn,Bdate,Address,Sex,Salary,Super_ssn,Dno)
+VALUES
+    ('Jake', 'B', 'Peralta', 281198123, '1956-01-09', '731 Baxton, Austin, TX', 'M', 40000, NULL, 2);
+
+-----------------------
+
+INSERT INTO Department
+    (Dname,Dnumber,Mgr_Ssn,Mgr_start_date)
+VALUES
+    ('NewDep', 2, 281198123, '1983-04-12');
+
+INSERT INTO Employee
+    (Fname,Minit,Lname,Ssn,Bdate,Address,Sex,Salary,Super_ssn,Dno)
+VALUES
+    ('Jake', 'B', 'Peralta', 281198123, '1956-01-09', '731 Baxton, Austin, TX', 'M', 40000, NULL, 2);
